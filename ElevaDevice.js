829ab449b5ad31a5dabc6fefccd9edd7130550c0
Gokul <118755346+gokulramalingam9585@users.noreply.github.com>
2022-12-09 11:28:47 +0530

    Add files via upload

---
+++ ElevaDevice.js
@@ -0,0 +1,90 @@
+
+const bcrypt = require('bcrypt');
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 9000,
+    user: 'postgres',
+    password: 'root',
+    database: 'test'
+});
+
+
+const getOccupants = (request, response) => {
+    const id = request.params.id
+    pool.query('select * from eleva.occupants_details where user_id = $1', [id], (error, result) => {
+        if (error) {
+            return response.status(400).json({
+                status: "Error",
+                reCode: 400,
+                msg: "Request Not Available",
+                isExist: false
+            })
+        }
+        if (!result.rows.length) {
+            response.status(200).json({
+                status: "Sucess",
+                reCode: 200,
+                response: `${id}`,
+                msg: "User Not Exist",
+                isExist: false
+            })
+        }
+        response.status(200).json(result.rows)
+    })
+}
+
+const getElevaEvents = (request, response) => {
+    const id = request.params.id
+    pool.query('select * from eleva.events_details where id = $1', [id], (error, result) => {
+        if (error) {
+            return response.status(400).json({
+                status: "Error",
+                reCode: 400,
+                msg: "Request Not Available",
+                isExist: false
+            })
+        }
+        if (!result.rows.length) {
+            return response.status(200).json({
+                status: "Sucess",
+                reCode: 200,
+                response: `${id}`,
+                msg: "Events Not Exisit",
+                isExist: false
+            })
+        }
+        response.status(200).json(result.rows)
+    })
+}
+
+const getEleva = (request, response) => {
+    const id = request.params.id
+    pool.query('select * from eleva.eleva_details where eleva_id = $1', [id], (error, result) => {
+        if (error) {
+           return response.status(400).json({
+            status: "Error", 
+            reCode: 400, 
+            msg: "Request Not Available",
+            isExist:false})
+        }
+        if (!result.rows.length) {
+            return response.status(200).json({ 
+                status: "sucess", 
+                reCode: 200, 
+                msg: "eleva Not Exist",
+                isExist:false })
+        }
+        else {
+            response.status(200).json(result.rows)
+        }
+    })
+}
+
+module.exports = {
+    getOccupants,
+    getElevaEvents,
+    getEleva
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const secretary = require('./secretary')
 const eleva = require('./elevator')
 const events = require('./events')
 const maintanence = require('./maintanence');
+const elevaDevice = require('./ElevaDevice');
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }))
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,7 @@ app.post('/secretary/register', secretary.creatUserSecretary)
 app.get('/secretary/user/:id',secretary.getUserSecretary)
 app.put('/secretary/user/:id',secretary.updateUserSecretary)
 
+
 app.get('/eleva/:id',eleva.getElevaDetails)
 app.get('/building/:id',eleva.getBuildingDetails)
 
@@ -29,6 +31,10 @@ app.delete('/delete_event/:id',events.deleteEvent)
 
 app.post('/shedule_maintanence',maintanence.createMaintanence)
 app.get('/eleva_maintanence/:id',maintanence.getMaintanence)
+
+app.get('/eleva_device/occupants/:id',elevaDevice.getOccupants)
+app.get('/eleva_device/events/:id',elevaDevice.getElevaEvents)
+app.get('/eleva_device/elevator/:id',elevaDevice.getEleva)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
