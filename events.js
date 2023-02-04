cabb83e30135b99eb5112316aca039afe38bd2f8
Gokul <118755346+gokulramalingam9585@users.noreply.github.com>
2023-02-04 13:14:54 +0530

    Add files via upload

--- events.js
+++ events.js
@@ -13,95 +13,235 @@ const pool = new Pool({
 });
 
 const creatEventsOccupants = (request, response) => {
-    const { id, title, details, date, from_time, to_time, location, status, cancel_reason, denied_reason, building_id, created_by, creator_id } = request.body
+    const { id, title, details, date, from_time, to_time, location, status, cancel_reason, denied_reason, building_id, created_by, creator_id, secretary_notes } = request.body
     console.log({ location });
 
-    pool.query('INSERT INTO eleva.events_details ( id,title,details,date,from_time,to_time,location,status,cancel_reason,denied_reason,building_id,created_by,creator_id ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)',
-        [id, title, details, date, from_time, to_time, location, status, cancel_reason, denied_reason, building_id, created_by, creator_id], (error, result) => {
+    pool.query('INSERT INTO eleva.events_details ( id,title,details,date,from_time,to_time,location,status,cancel_reason,denied_reason,building_id,created_by,creator_id,secretary_notes ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13)',
+        [id, title, details, date, from_time, to_time, location, status, cancel_reason, denied_reason, building_id, created_by, creator_id, secretary_notes], (error, result) => {
             if (error) {
-                return response.status(400).json({
+                response.status(400).json({
                     status: "Error",
                     reCode: 400,
                     msg: "Event Not Created Sucessfully",
                     isExist: false
                 })
+                return
             }
             response.status(200).json({
                 status: "Sucess",
                 reCode: 200,
-                msg: `Event Created successfully with Id : ${id}`
+                response: `${title}`,
+                msg: `Event Created successfully`
             });
         })
 }
 
 const getEvents = (request, response) => {
     const id = request.params.id
+
     pool.query('select * from eleva.events_details where id = $1', [id], (error, result) => {
         if (error) {
-            return response.status(400).json({
+            response.status(400).json({
                 status: "Error",
                 reCode: 400,
                 msg: "Request Not Available",
                 isExist: false
             })
+            return
         }
         if (!result.rows.length) {
-            return response.status(200).json({
+            response.status(200).json({
                 status: "Sucess",
                 reCode: 200,
                 response: `${id}`,
-                msg: "Events Not Exisit",
+                msg: "Event Not Exisit",
                 isExist: false
             })
-        }
-        response.status(200).json(result.rows)
-    })
-}
-
-const getAllEvents = (request, response) => {
-    const date = request.params.date
-    pool.query('select * from eleva.events_details where date >= $1', [date], (error, result) => {
-        if (error) {
-            return response.status(400).json({
-                status: "Error",
-                reCode: 400,
-                msg: "Request Not Available",
-                isExist: false
-            })
-        }
-        if (!result.rows.length) {
-            return response.status(200).json({
-                status: "Sucess",
-                reCode: 200,
-                response: `${date}`,
-                msg: "Events Not Available",
-                isExist: false
-            })
+            return
         }
         response.status(200).json({
             status: "Sucess",
             reCode: 200,
             msg: "Events Available",
             isExist: true,
-            response: result.rows 
+            response: result.rows
         })
+
     })
+
 }
 
+
+
+const getAllEvents = (request, response) => {
+    const date = request.params.date
+    const id = request.params.id
+    const created_by = request.params.created_by
+    const user_id = request.params.user_id
+    const secretary_id = request.params.secretary_id
+    const status = request.params.status + ''
+    pool.query('select * from eleva.events_details where building_id = $1 and date >= $2 and status = $3', [id, date, status], (error, result) => {
+        if (error) {
+            response.status(400).json({
+                status: "Error",
+                reCode: 400,
+                msg: "Request Not Available",
+                isExist: false
+            })
+            return
+        }
+        if (!result.rows.length) {
+            response.status(200).json({
+                status: "Sucess",
+                reCode: 200,
+                response: `${date}`,
+                msg: "Events Not Available",
+                isExist: false
+            })
+            return
+        }
+        if (created_by == 'user') {
+            pool.query('select * from eleva.occupants_details where user_id = $1', [user_id], (error, res) => {
+                if (error) {
+                    response.status(400).json({
+                        status: "Error",
+                        reCode: 400,
+                        msg: "Request Not Available",
+                        isExist: false
+                    })
+                    return;
+                }
+
+                response.status(200).json({
+                    response: res.rows + response.status(200).json({
+                        status: "Sucess",
+                        reCode: 200,
+                        msg: "Events Available",
+                        isExist: true,
+                        response: result.rows
+                    })
+                })
+            })
+        } else if (created_by == 'secretary') {
+            pool.query('select * from eleva.secretary_details where secretary_id = $1', [secretary_id], (error, res) => {
+                if (error) {
+                    response.status(400).json({
+                        status: "Error",
+                        reCode: 400,
+                        msg: "Request Not Available",
+                        isExist: false
+                    })
+                    return
+                }
+                response.status(200).json({
+                    response: res.rows + response.status(200).json({
+                        status: "Sucess",
+                        reCode: 200,
+                        msg: "Events Available",
+                        isExist: true,
+                        response: result.rows
+                    })
+                })
+            })
+        }
+    })
+}
 
 const deleteEvent = (request, response) => {
     const id = request.params.id;
 
     pool.query('DELETE FROM eleva.events_details WHERE id = $1', [id], (error, result) => {
         if (error) {
-            return response.status(400).json({
+            response.status(400).json({
                 status: "Error",
                 reCode: 400,
                 msg: "Not Deleted Sucessfully",
                 isExist: false
             })
+            return
         }
-        response.status(200).json({ status: "sucess", reCode: 200, Msg: `Event deleted with : ${id}` })
+        response.status(200).json({
+            status: "sucess",
+            reCode: 200,
+            response: `${id}`,
+            Msg: 'Event Deleted Sucessfully'
+        })
+    })
+}
+
+const updateEventStatus = (request, response) => {
+
+    const { id, denied_reason, secretary_notes, status, cancel_reason } = request.body
+
+    if (status == 'accepted') {
+        pool.query('update eleva.events_details set secretary_notes = $1, status = $2 where id = $3', [secretary_notes, status, id], (error, result) => {
+            if (error) {
+                response.status(400).json({
+                    status: false,
+                    reCode: 400,
+                    msg: "Event Not Updated"
+                })
+                return
+            }
+            response.status(200).json({
+                status: true,
+                reCode: 200,
+                msg: `Event Accepted`
+            })
+        })
+    } else if (status == 'rejected') {
+        pool.query('update eleva.events_details set status = $1,  denied_reason = $2 where id = $3', [status, denied_reason, id], (error, result) => {
+            if (error) {
+                response.status(400).json({
+                    status: false,
+                    reCode: 400,
+                    msg: "Event Not Updated"
+                })
+                return
+            }
+            response.status(200).json({
+                status: true,
+                reCode: 200,
+                msg: `Event Denied`
+            })
+        })
+    } else if (status == 'cancelled') {
+        pool.query('update eleva.events_details set status = $1, cancel_reason = $2 where id = $3', [status, cancel_reason, id], (error, result) => {
+            if (error) {
+                return response.status(400).json({
+                    status: false,
+                    reCode: 400,
+                    msg: "Event Not Updated"
+                })
+            }
+            response.status(200).json({
+                status: true,
+                reCode: 200,
+                msg: `Event Cancelled`
+            })
+        })
+    }
+}
+
+const updateEventDetails = (request, response) => {
+
+    const { title, details, date, from_time, to_time, location, id } = request.body
+    console.log({ location });
+
+    pool.query('update eleva.events_details set title = $1,  details = $2,date = $3,from_time = $4, to_time = $5, location = $6 where id = $7', [title, details, date, from_time, to_time, location, id], (error, result) => {
+        if (error) {
+            response.status(400).json({
+                status: false,
+                reCode: 400,
+                msg: "Event Not Updated"
+            })
+            return
+        }
+        response.status(200).json({
+            status: true,
+            reCode: 200,
+            msg: `Event updated successfully`
+        })
     })
 }
 
@@ -111,5 +251,7 @@ module.exports = {
     creatEventsOccupants,
     getEvents,
     deleteEvent,
-    getAllEvents
+    getAllEvents,
+    updateEventStatus,
+    updateEventDetails
 }
--- index.js
+++ index.js
@@ -13,30 +13,32 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/occupants/:phone_number', occupants.CheckuserOccupants)
 app.post('/occupants/register', occupants.creatUserOccupants)
-app.get('/occupants/user/:id',occupants.getUserOccupants)
-app.get('/occupants/getalloccupants/:id',occupants.getBuildingOccupants)
-app.put('/occupants/user/:id',occupants.updateUserOccupants)
+app.get('/occupants/user/:user_id',occupants.getUserOccupants)
+app.get('/occupants/getalloccupants/:building_id',occupants.getBuildingOccupants)
+app.put('/occupants/user/:user_id',occupants.updateUserOccupants)
 
 app.get('/secretary/:phone_number', secretary.checkUserSecretary)
 app.post('/secretary/register', secretary.creatUserSecretary)
-app.get('/secretary/user/:id',secretary.getUserSecretary)
-app.put('/secretary/user/:id',secretary.updateUserSecretary)
+app.get('/secretary/user/:secretary_id',secretary.getUserSecretary)
+app.put('/secretary/user/:secretary_id',secretary.updateUserSecretary)
 
 
 app.get('/eleva/:id',eleva.getElevaDetails)
-app.get('/building/:id',eleva.getBuildingDetails)
+app.get('/building/:building_id',eleva.getBuildingDetails)
 
 app.post('/create_event',events.creatEventsOccupants)
 app.get('/events/:id',events.getEvents)
-app.get('/events/getallevents/:date',events.getAllEvents)
+app.get('/events/getallevents/:building_id/:date',events.getAllEvents)
 app.delete('/delete_event/:id',events.deleteEvent)
+app.put('/events/update/:id',events.updateEventStatus)
+app.put('/events/updatedetails/:id',events.updateEventDetails)
 
-app.post('/shedule_maintanence',maintanence.createMaintanence)
+app.post('/schedule_maintanence',maintanence.createMaintanence)
 app.get('/eleva_maintanence/:id',maintanence.getMaintanence)
 
-app.get('/eleva_device/occupants/:id',elevaDevice.getOccupants)
+app.get('/eleva_device/occupants/:building_id',elevaDevice.getOccupants)
 app.get('/eleva_device/events/:id',elevaDevice.getElevaEvents)
-app.get('/eleva_device/elevator/:id',elevaDevice.getEleva)
+app.get('/eleva_device/elevator/:eleva_id',elevaDevice.getEleva)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
